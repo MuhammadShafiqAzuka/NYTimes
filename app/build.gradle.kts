572e014351ee12faeda033aab6804db391b58ea4
shafiq <shafiq@xentralmethods.com>
2024-07-24 10:38:50 +0800

    add search response

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.nytimes"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 30
         versionCode = 1
         versionName = "1.0"
 
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.testng)
+    implementation(libs.androidx.junit.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -54,4 +56,32 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+
+    // Mockito for mocking
+    testImplementation (libs.mockito.core)
+    testImplementation (libs.mockito.inline)
+
+    // Coroutines testing
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.5.1")
+
+    // AndroidX Test - Instrumentation testing
+    androidTestImplementation ("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.6.1")
+
+    // Core library
+    testImplementation ("androidx.arch.core:core-testing:2.2.0")
+    testImplementation ("androidx.recyclerview:recyclerview:1.2.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.4.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-intents:3.4.0")
+
+    // AndroidX Test - Core library
+    androidTestImplementation ("androidx.test:core:1.4.0")
+
+    // AndroidX Test - Rules
+    androidTestImplementation ("androidx.test:rules:1.4.0")
+
+    // AndroidX Test - Runner
+    androidTestImplementation ("androidx.test:runner:1.4.0")
 }
